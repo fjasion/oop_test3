1c7cfe017b2404dd7d5baa93582c42a648999248
FJ <filip.jasionowicz@student.uj.edu.pl>
2022-04-22 16:59:54 +0200

    qwerty2

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -4,6 +4,8 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("Some shit");
+
+        System.out.println("More shit");
         Lwjgl3ApplicationConfiguration q = new Lwjgl3ApplicationConfiguration();
         q.disableAudio(false);
     }
