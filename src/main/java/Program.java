9520c2040a7601f54874ad84cb7e2f6fa1972b3e
FJ <filip.jasionowicz@student.uj.edu.pl>
2022-04-22 16:49:40 +0200

    Lwjgl_test

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,5 +1,9 @@
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        Lwjgl3ApplicationConfiguration q = new Lwjgl3ApplicationConfiguration();
+        q.disableAudio(false);
     }
 }
