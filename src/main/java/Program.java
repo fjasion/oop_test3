dc9705a9af617a2fd8b39c8a97f0d14245e3456a
FJ <filip.jasionowicz@student.uj.edu.pl>
2022-04-22 16:57:01 +0200

    qwerty

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,6 +3,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println("Some shit");
         Lwjgl3ApplicationConfiguration q = new Lwjgl3ApplicationConfiguration();
         q.disableAudio(false);
     }
